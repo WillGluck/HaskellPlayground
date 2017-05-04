6cd2c44c4aba191c569f121c3987340002b6303e
Will Glück <william.gluck@teclogica.com.br>
2017-05-04 09:25:24 -0300

    Funções bobas
    Change-Id: Ib362195c08e167ebeae01bc1909ffdebd990c208

---
+++ playgroundFunctions.hs
@@ -0,0 +1,9 @@
+-- Funções bobas para brincar um pouco
+
+doubleMe x = x + x
+
+doubleUsAndSum x y = x*2 + y*2
+
+doubleBelow100 x = if x > 100 then x else x*2
+
+doubleBelow100' x = (if x > 100 then x else x*2) + 1
