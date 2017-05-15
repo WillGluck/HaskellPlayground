ccb9ba8c2fa03a0e6b62d303ed2f54bfb78c5c2c
Will Glück <william.gluck@teclogica.com.br>
2017-05-15 16:31:12 -0300

    Modules
    Testing Haskell modules system
    
    Change-Id: I829268c426045d9d7f4ba775ecbbb48a18ad24b6

---
+++ Cube.hs
@@ -0,0 +1,12 @@
+--Módulo para cálculos geométricos de um cubo.
+
+module Geometry.Cube
+( volume
+, area
+) where
+
+volume :: Float -> Float
+volume radius = (4.0 / 3.0) * pi * (radius ^ 3)
+
+area :: Float -> Float
+area radius = 4 * pi * (radius ^ 2)
---
+++ randomPlayground.hs
@@ -0,0 +1,17 @@
+--Modules.
+
+--import Data.List
+-- :m + Data.List Data.Map Data.Set --No GHCI
+-- import Data.List (nub, sort) --Métodos específicos
+-- import Data.List hiding (nub) --Retira método específico
+-- import qualified Data.Map as M -- Com namespace (customizado)
+
+import Data.List
+import Data.Char
+import Data.Map
+import Data.Set
+
+
+import Geometry.Sphere as Sphere
+import Geometry.Cuboid as Cuboid
+import Geometry.Cube as Cube
