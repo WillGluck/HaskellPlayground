46c9786c8e95d7786e9db9a78f378b15475eccbf
Will Glück <william.gluck@teclogica.com.br>
2017-05-16 12:56:14 -0300

    Anotações sobre funcões do Módulo Data.List
    Anotações e pquenos exemplos do módulo Data.List
    
    Change-Id: Ic3a67ab84a52f74ff3529696f64dc2ff566e7b48

--- randomPlayground.hs
+++ randomPlayground.hs
@@ -11,6 +11,8 @@ import Data.Char
 import Data.Map
 import Data.Set
 
+--DATA.LIST
+
 -- interperse - Passa um valor de uma lista. Adiciona o valor no meio de cada item da lista.
 -- intercalate - Passa uma lista e uma lista de listas. Estica os valores, adicionando a lista passada no meio das listas que constituiam a outra lisca (sic!)
 -- transpose - Sem segredo, pega uma lista de listas e transforma as "colunas" viram as "linhas"
@@ -37,7 +39,7 @@ let (a,b) = splitAt 4 "WILLGLUK" in b ++ a --invertendo a string na maciota
 -- dropWhile - Parecida com takeWhile, mas em vez de "pegar" enquanto for true, remove da lista enquanto for true
 -- span - recebe uma função e uma lista também, mas retorna duas listas. Uma é o resultado de um takeWhile e a outra são os que "caíram fora" da validação do takeWhile
 -- sort - recebe uma lista de Ord e ordena
--- group - pega uma lista de divide em uma lista de listas com todos os itens que são iguais. Agrupa os iguais, basicamente
+-- group - pega uma lista de divide em uma lista de listas com todos os itens que são iguais. Agrupa os iguais, basicamente (por ordem)
 -- inits e tails - cria uma lista recursivamente aplicando init e tail, até não sobrar mais nada. Retorna uma lista dos resultados parciais.
 search :: (Eq a) =>  [a]-> [a] -> Bool
 search needle haystack =
@@ -46,6 +48,34 @@ search needle haystack =
 -- isInfixOf - basicamente a implementação acima. Procura uma lista dentro de uma lista. Tem isPrefixOf e isSuffixOf que vem o início e final, respectivamente.
 -- elem e notElem verifica se um item está ou não, respectivamente, em uma lista.
 -- partition - Recebe uma lista e predicado. Retorna primeira a lista que satisfaz o predicado e dps a que não.
+-- find - Recebe um predicado e retorna um Maybe do primeiro encontrado. Maybe pode ser Just alguma coisa ou Nothing.
+-- elemIndex - Tipo elem, mas em vez de retornar boolean retorna um Maybe do índice do primeiro encontrado.
+-- elemIndices - Igual elemIndex mas retorna uma lista de índices. Serve pra encontrar vários.
+-- findIndex - Tipo find, só que retorna uma lista de índices. Serve pra encontrar vários.
+-- zip3, zip4, zipWith3, zipWith4...vai até 7.
+-- lines - pra lidar com textos. Pega cada linha de texto e coloca em uma lista diferente. Retorna uma lista de listas dai.
+-- unlines - Função inversa de lines.
+-- words e unwords - divide o texto em palavras e o contrário, respectivamente.
+-- delete - deleta a primeira ocorrência do valor passado da lista passada.
+-- \\ - Usa LISTA \\ LISTA - cria uma nova lista sem os elementos que existem na lista da esquerda e direita (exclusão é um pra um)
+-- union - Junta duas listas mas remove repetições!
+ -- intersect - cria nova lista apenas com os valores que se repetem em ambas as listas.
+ -- insert - adiciona um valor numa lista que pode ser ordenada, na primeira posição "correta" encontrada (baseada na ordenação).
+ -- length, take, drop, splitAt, !! e replicate usam INTS por motivos históricos
+ -- Para casos diferentes usar genericNOME_DO_METODO (exceção sendo genericIndex que representa a função !!)
+ -- nub, delete, union, intersect e group usam == para as comparações
+ -- Quando quiser comparação customizada usar nubBy, deleteBy, unionBy, intersectBy e groupBy. usar group é igual usar groupBy (==)
+ separatePositivesAndNegatives = groupBy (\x y -> (x > 0) == (y > 0))
+-- on
+separatePositivesAndNegatives' = groupBy ((==) `on` (>))
+-- sort, insert, maximum e minimum também podem ser usados pra ordenação customizada com sortBy, insertBy, maximumBy e minimumBy.
+-- Como saber onde adicionar uma lista de listas? Se uma lista não é Ord
+let xs = [[5,4,5,4,4], [1,2,3], [3,5,4,3], [], [2], [2,2]]
+sortBy (compare `on` length) xs
+-- Resultado [[],[2],[2,2],[1,2,3],[3,5,4,3],[5,4,5,4,4]]
+
+--DATA.CHAR
+
 
 
 import Geometry.Sphere as Sphere
