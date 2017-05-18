a2d778b09676a38511474d2b8e102a7230ff783d
willgluck <bigwillgluck@gmail.com>
2017-05-17 22:31:42 -0300

    Anotações sobre funcões do Módulo Data.Set
    Anotações e pquenos exemplos do módulo Data.Set

--- randomPlayground.hs
+++ randomPlayground.hs
@@ -8,33 +8,38 @@
 
 import Data.List
 import Data.Char
-import Data.Map
+import qualified Data.Map as Map
 import Data.Set
+import Data.Function
+
+--import Geometry.Sphere as Sphere
+--import Geometry.Cuboid as Cuboid
+--import Geometry.Cube as Cube
 
 --DATA.LIST
 
 -- interperse - Passa um valor de uma lista. Adiciona o valor no meio de cada item da lista.
 -- intercalate - Passa uma lista e uma lista de listas. Estica os valores, adicionando a lista passada no meio das listas que constituiam a outra lisca (sic!)
 -- transpose - Sem segredo, pega uma lista de listas e transforma as "colunas" viram as "linhas"
-let equation1 = [0,3,5,9]
-let equation2 = [10,0,0,9]
-let equation3 = [8,5,1,-1]
-let equations = [equation1, equation2, equation3]
-let equationsSum = map sum $ transpose equations
+equation1 = [0,3,5,9]
+equation2 = [10,0,0,9]
+equation3 = [8,5,1,-1]
+equations = [equation1, equation2, equation3]
+equationsSum = Data.List.map sum $ transpose equations
 -- foldl' e foldl1' - são as versões estrítas
 -- concat - apesar do nome, apenas faz um flat em uma lista de listas...
 -- concatMap - mesma coisa que fazer um map e dps concat
 -- and - recebe uma lista de booleans e retorna true se todos forem true
 -- or - recebe uma lista de booleans e retorna true se um deles for true
-and $ map (>3) [1,2,3,4,5]
-or $ map (==4) [1,2,3,4,5]
+asd = and $ Data.List.map (>3) [1,2,3,4,5]
+asdqwe = or $ Data.List.map (==4) [1,2,3,4,5]
 -- any e all - recebe um predicado e verifica se algum ou todos elementos satisfazem o predicado, respectivamente. É usado normalmente ao invés da combinação MAP + (OR/AND)
-all (>3) [1,2,3,4,5]
-any (==4) [1,2,3,4,5]
+afaqwe = all (>3) [1,2,3,4,5]
+ert = any (==4) [1,2,3,4,5]
 --iterate - Pega uma função de um valor inicial e vai acumulando ele eternamente... Daí usa um take, por exemplo pra pegar o valor.
-take 10 $ iterate (*2) 1
+rty = take 10 $ iterate (*2) 1
 -- splitAt - pega um número e quebra uma lista em duas tuplas com cada metade.
-let (a,b) = splitAt 4 "WILLGLUK" in b ++ a --invertendo a string na maciota
+poi = let (a,b) = splitAt 4 "WILLGLUK" in b ++ a --invertendo a string na maciota
 -- takeWhile - conhecida já
 -- dropWhile - Parecida com takeWhile, mas em vez de "pegar" enquanto for true, remove da lista enquanto for true
 -- span - recebe uma função e uma lista também, mas retorna duas listas. Uma é o resultado de um takeWhile e a outra são os que "caíram fora" da validação do takeWhile
@@ -44,7 +49,7 @@ let (a,b) = splitAt 4 "WILLGLUK" in b ++ a --invertendo a string na maciota
 search :: (Eq a) =>  [a]-> [a] -> Bool
 search needle haystack =
   let nlen = length needle
-  in foldl (\acc x -> if take nlen x == needle then True else acc) False (tails haystack)
+  in Data.List.foldl (\acc x -> if take nlen x == needle then True else acc) False (tails haystack)
 -- isInfixOf - basicamente a implementação acima. Procura uma lista dentro de uma lista. Tem isPrefixOf e isSuffixOf que vem o início e final, respectivamente.
 -- elem e notElem verifica se um item está ou não, respectivamente, em uma lista.
 -- partition - Recebe uma lista e predicado. Retorna primeira a lista que satisfaz o predicado e dps a que não.
@@ -65,13 +70,13 @@ search needle haystack =
  -- Para casos diferentes usar genericNOME_DO_METODO (exceção sendo genericIndex que representa a função !!)
  -- nub, delete, union, intersect e group usam == para as comparações
  -- Quando quiser comparação customizada usar nubBy, deleteBy, unionBy, intersectBy e groupBy. usar group é igual usar groupBy (==)
- separatePositivesAndNegatives = groupBy (\x y -> (x > 0) == (y > 0))
+separatePositivesAndNegatives = Data.List.groupBy (\x y -> (x > 0) == (y > 0))
 -- on
 separatePositivesAndNegatives' = groupBy ((==) `on` (>))
 -- sort, insert, maximum e minimum também podem ser usados pra ordenação customizada com sortBy, insertBy, maximumBy e minimumBy.
 -- Como saber onde adicionar uma lista de listas? Se uma lista não é Ord
-let xs = [[5,4,5,4,4], [1,2,3], [3,5,4,3], [], [2], [2,2]]
-sortBy (compare `on` length) xs
+xs = [[5,4,5,4,4], [1,2,3], [3,5,4,3], [], [2], [2,2]]
+gfh = Data.List.sortBy (compare `on` length) xs
 -- Resultado [[],[2],[2,2],[1,2,3],[3,5,4,3],[5,4,5,4,4]]
 
 --DATA.CHAR
@@ -79,29 +84,48 @@ sortBy (compare `on` length) xs
 -- isControl (verifica se é caracter de controle), isSpace, isLower, isUpper, isAlpha (letra), isAlphaNum (número e letra), isPrint,
 -- isDigit, isOctoDigit (é um dígito octal), isHexDigit, isLetter, isMark (verifica se é um caracter unicode de marcação..francês),
 -- isNumber, isPunctuation, isSymbol, isSeparator (espaços e separadores), isAscii, isAsciiUpper, isAsciiLower...
-all isAlphaNum "bobby283"
+tyu = all isAlphaNum "bobby283"
 --True
 --Simulando words com isSpace
-filter (not. any isSpace) . groupBy ((==) `on` isSpace)) $  "hey guys its me"
+wefv = Data.List.filter (not. any Data.Char.isSpace) . Data.List.groupBy ((==) `on` Data.Char.isSpace) $  "hey guys its me"
 -- ["hey", "guys", "its", "me"]
 -- generalCategory - identifica a GeneralCategory (extende de Eq) de um caracter. É um enumerador
-generalCategory 'A'
+sdfsa = generalCategory 'A'
 -- UppercaseLetter
 -- Para manipular: toUpper, toLower, toTitle (as vzs é UpperCase), digitToInt (0..9) (a..f), intToDigit.
 -- Ord retorna o valor numérico que representa o caracter. chr retorna o char de um valor numérico
 encode :: Int -> String -> String
 encode shift text =
-  let ords = map ord text
-      shifted = map (+shift) ords
-  in map chr shifted
+  let ords = Data.List.map ord text
+      shifted = Data.List.map (+shift) ords
+  in Data.List.map chr shifted
 
 decode :: Int -> String -> String
 decode shift text = encode (negate shift) text
 
 --DATA.MAP
 
+-- uma maneira de representar maps? Association List [(a,b)]
+findKey :: (Eq k) => k -> [(k, v)] -> Maybe v
+--findKey key [] = Nothing
+--findKey key ((k,v):xs) = if key == k then Just v else findKey key xs
+findKey key  = Data.Set.foldr (\(k,v) acc -> if key == k then Just v else acc) Nothing
+-- Mapas são mais otimizados. Use fromList para transformar uma lista associativa em um Map. Remove chaves duplicadas.
+-- Map.fromList ::(Ord k) => [(k,v)] -> Map.Map k v
+--empty - retorna map vazio
+-- insert - recebe chave, valor e um mapa e faz o trabalho
+-- null - verifica se o mapa é nulo
+-- size - Retorna o tamanho do mapa
+-- singleton - recebe chave valor e cria um mapa com apenas um valor Oo
+-- lookup - igual o do Data.List. Retorna Just valor ou Nothing.
+-- member - recebe a key e o mapa e retorna boolean dizendo se a chave está
+-- map e filter - igual o da lista, operam apenas no valor.
+-- toList - inverso do fromList
+-- keys e elems retorna uma lista com as chaves e elementos, respectivamente.
+-- fromListWith - recebe uma função para decidir o que fazer com chaves duplicadas.
 
-
-import Geometry.Sphere as Sphere
-import Geometry.Cuboid as Cuboid
-import Geometry.Cube as Cube
+--phoneBookToMap :: (Ord k) => [(k,String)] -> Map.Map k String
+--phoneBookToMap xs = Map.fromListWith (\number1 number2 -> number1 ++ ", " ++ number2) xs
+phoneBookToMap :: (Ord k) => [(k, a)] -> Map.Map k [a]
+phoneBookToMap xs = Map.fromListWith (++) $ Data.Set.map (\(k,v) -> (k, [v])) xs -- transforma valor em lista e aplica somma de listas.
+--insertWith - o que fromListWith é para fromList insertWith é para insert. No caso de valores repetidos.
