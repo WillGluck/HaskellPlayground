b2f5ad820c34d6ca932d405b0119c0d8ae3d0f24
Will Glück <william.gluck@teclogica.com.br>
2017-05-26 12:47:18 -0300

    Recursive Data Structures
    Change-Id: I8e37db9d939ac901c14e4b5d4b1f5d4089402b12

--- typesPlayground.hs
+++ typesPlayground.hs
@@ -139,3 +139,41 @@ lockerLookup lockerNumber lockerMap =
 
 
 -- Recursive Data Structures
+
+--data List a = Empty | Cons a (List a) deriving (Show, Read, Eq, Ord) -- Cons is like ":" of lists
+--data List a = Empty | Cons { listHead :: a, listTail :: List a} deriving (Show, Read, Eq, Ord) -- Verbose way
+infixr 5 :-: -- infixr - right associative, fixity value is 5.
+data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)
+
+--infixr 5 ++
+--(++) :: [a] -> [a] -> [a]
+--[] ++ ys = ys
+--(x:xs) ++ ys = x : (x ++ ys)
+
+infixr 5 .++
+(.++) :: List a -> List a -> List a
+Empty .++ ys = ys
+(x :-: xs) .++ ys = x :-: (xs .++ ys)
+-- Pattern matching is about matching constructors. It work only on constructors. 8, 'a' are constructors for numeric and character types.
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree =  singleton x
+treeInsert x (Node a left right)
+  | x == a = Node x left right
+  | x < a = Node a (treeInsert x left) right
+  | x > a = Node a left (treeInsert x right)
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+  | x == a = True
+  | x < a = treeElem x left
+  | x > a = treeElem x right
+
+
+-- Typeclasses 102
