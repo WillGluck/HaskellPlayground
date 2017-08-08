5f2148310e9d13190dc0e1e541ab0e1f454ac723
Will Glück <william.gluck@teclogica.com.br>
2017-08-08 12:51:53 -0300

    Exercising pt 2
    Change-Id: I49bd28363931212a85cdbcf707351831111b6007

--- solvingProblems.hs
+++ solvingProblems.hs
@@ -27,5 +27,42 @@ data Label = A | B | C deriving (Show)
 type Path = [(Label, Int)]
 
 optimalPath  :: RoadSystem -> Path
-optimalPath = ...
+optimalPath = 
 
+roadStep :: (Path, Path) -> Section -> (Path, Path)
+roadStep (pathA, pathB) (Section a b c) =
+  let priceA = sum $ map snd pathA
+      priceB = sum $ map snd pathB
+      forwardPriceToA = priceA + A
+      crossPriceToA = priceB + c + b
+      forwardPriceToB = priceB + B
+      crossPriceToB = priceA + c + a
+      newPathToA = if forwardPriceToA <= crossPriceToA
+                      then (A,a):pathA
+                      else (C,c):(B:b):pathB
+      newPathToB = if forwardPriceToB <= crossPriceToB
+                      then (B,b):pathB
+                      else (C,c):(A,a):pathA
+  in (newPathToA, newPathToB)
+
+optimalPath :: RoadSystem -> Path
+optimalPath roadSystem = 
+  let (bestAPath, bestBPath) = foldl roadStep ([],[]) roadSystem
+  in if sum (map snd bestAPath) <= sum (map snd bestBPath)
+      then reverse bestAPath
+      else reverse bestBPath
+
+groupsOf :: Int -> [a] -> [[a]]
+groupsOf 0 _ = undefined
+groupsOf _ [] = []
+groupsOf n xs = take n xs : groupsOf n (drop n xs)
+
+main = do
+  contents <- getContents
+  let threes = groupsOf 3 (map read $ lines contents)
+      roadSystem = map (\[a,b,c] -> Section a b c) threes
+      path = optimalPath roadSystem
+      pathString = concat $ map (show . fst) path
+      pathPrice = sum $ map snd path
+  putStrLn $ "The best path to take is: " ++ pathString
+  putStrLn $ "The price is: " ++ show pathPrice
